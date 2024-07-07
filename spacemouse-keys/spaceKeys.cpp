9a05bb5663dff110e9e76ff2c7ef221d792b560a
AndunHH <76771247+AndunHH@users.noreply.github.com>
2024-07-07 10:32:58 +0200

    Send HID data with constant rate (#34)
    Based on the reverse engineered SpaceNavigator, see SpaceNavigator.md, this commit introduces a little state machine, which sends new data over the usb interface with a constant rate every 8 ms. This is not to be configured by every user and therefore not in the config_sample.h. If no data is to sent, the zero data are sent three times before stopping the transmissions.
    
    * Introduced debounced keyState for key evaluation
    * Changed the HID descriptor to a symmetric logical range of +/- 350. It has been -342 to +350, despite the comment saying it to be -350 all the time. 0xFEA2 = 350, not 0xFEAA. ?!
    * Changed HID report from absolute to relative values to avoid the need for jiggling the values. This is needed because equal absolute inputs are ignored in linux. Check the discussion in spacenavd for more informations:
    https://github.com/FreeSpacenav/spacenavd/issues/108#issuecomment-2209440745

--- spacemouse-keys/spaceKeys.cpp
+++ spacemouse-keys/spaceKeys.cpp
@@ -9,7 +9,6 @@
 int keyList[NUMKEYS] = KEYLIST;
 
 //Needed for key evaluation
-int8_t key_waspressed[NUMKEYS];
 unsigned long timestamp[NUMKEYS];
 
 // Function to setup up all keys in keyList
@@ -27,19 +26,18 @@ void readAllFromKeys(int* keyVals) {
   }
 }
 
-// Evaluate and debounce all keys from the raw keyVals into the debounced keyOut.
+// Evaluate and debounce all keys from the raw keyVals into the debounced keyOut event or the debounced keyState.
 // The keyOut is only 1 for one iteration of the loop.
-// If you want to know, if the key is pressed all the time (for the kill-key feature), use the not debounced raw keyVal!
-void evalKeys(int* keyVals, uint8_t* keyOut) {
+void evalKeys(int* keyVals, uint8_t* keyOut, uint8_t* keyState) {
   //Button Evaluation
   for (int i = 0; i < NUMKEYS; i++) {
     // The keys are configured with pull_up, see setupKeys() and are pulled to ground, when pressed.
     // Therefore, the pressed key is false, which is an inverted logic
     if (!keyVals[i]) {  // the key is pressed
       // Making sure button cannot trigger multiple times which would result in overloading HID.
-      if (key_waspressed[i] == 0) {  // if the button has not been pressed lately:
+      if (keyState[i] == 0) {  // if the button has not been pressed lately:
         keyOut[i] = 1;               // this is the variable telling the outside world only one iteration, that the key was pressed
-        key_waspressed[i] = 1;       // remember, that we already told the outside world about this key
+        keyState[i] = 1;       // remember, that we already told the outside world about this key
         timestamp[i] = millis();     // remember the time, the button was pressed
         Serial.print("Key: ");       // this is always sent over the serial console, and not only in debug
         Serial.println(i);
@@ -47,10 +45,10 @@ void evalKeys(int* keyVals, uint8_t* keyOut) {
         keyOut[i] = 0;
       }
     } else {                         // the button is not pressed
-      if (key_waspressed[i] == 1) {  // has it been pressed lately?
+      if (keyState[i] == 1) {  // has it been pressed lately?
         // debouncing:
         if (millis() - timestamp[i] > DEBOUNCE_KEYS_MS) {  // check if the last button press is long enough in the past
-          key_waspressed[i] = 0;                           // reset this marker and allow a new button press
+          keyState[i] = 0;                           // reset this marker and allow a new button press
         }
       }
     }
