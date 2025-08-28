a4e479e2b673d6fd3e8d80249f085c0b7af1c351
AndunHH <76771247+AndunHH@users.noreply.github.com>
2025-08-28 22:01:37 +0200

    change to space mouse wireless

--- set_hwids.py
+++ set_hwids.py
@@ -3,6 +3,6 @@
 
 Import("env")
 board_config = env.BoardConfig()
-board_config.update("build.hwids", [["0x256f", "0xc631"]])
-board_config.update("build.usb_product", "SpaceMouse Pro Wireless (cabled)")
+board_config.update("build.hwids", [["0x256f", "0xc62e"]])
+board_config.update("build.usb_product", "SpaceMouse Wireless")
 board_config.update("vendor", "3Dconnexion")
