6f3976b5dfaec73eb1a590dbf7a1a3e58b1b3977
AndunHH <76771247+AndunHH@users.noreply.github.com>
2025-08-29 20:01:55 +0200

    reenable ADV_HID_REL. Bytes for buttons are fixed in hid descriptor now.

--- spacemouse-keys/SpaceMouseHID.h
+++ spacemouse-keys/SpaceMouseHID.h
@@ -18,10 +18,9 @@ This code is based on https://forum.arduino.cc/t/solved-unable-to-receive-hid-re
 #include "PluggableUSB.h"
 #include "HID.h"
 
-#define SPACEMOUSE_D_HIDREPORT(length)                                     \
-    {                                                                      \
-        9, 0x21, 0x11, 0x01, 0, 1, 0x22, lowByte(length), highByte(length) \
-    }
+#define SPACEMOUSE_D_HIDREPORT(length) \
+    {                                  \
+        9, 0x21, 0x11, 0x01, 0, 1, 0x22, lowByte(length), highByte(length)}
 typedef struct
 {
     InterfaceDescriptor hid;
@@ -33,38 +32,42 @@ typedef struct
 // The USB VID and PID for this emulated space mouse pro must be set in the boards.txt in arduino IDE or in set_hwids.py in platformIO.
 
 static const uint8_t SpaceMouseReportDescriptor[] PROGMEM = {
-    0x05, 0x01,       // Usage Page (Generic Desktop)
-    0x09, 0x08,       // Usage (Multi-Axis)
-    0xA1, 0x01,       // Collection (Application)
-                      // Report 1: Translation
-0xA1, 0x00,        //   Collection (Physical)
-0x85, 0x01,        //     Report ID (1)
-0x16, 0xA2, 0xFE,  //     Logical Minimum (-350)
-0x26, 0x5E, 0x01,  //     Logical Maximum (350)
-0x36, 0x88, 0xFA,  //     Physical Minimum (-1400)
-0x46, 0x78, 0x05,  //     Physical Maximum (1400)
-0x55, 0x0C,        //     Unit Exponent (-4)
-0x65, 0x11,        //     Unit (System: SI Linear, Length: Centimeter)
-0x09, 0x30,        //     Usage (X)
-0x09, 0x31,        //     Usage (Y)
-0x09, 0x32,        //     Usage (Z)
-0x09, 0x33,        //     Usage (Rx)
-0x09, 0x34,        //     Usage (Ry)
-0x09, 0x35,        //     Usage (Rz)
-0x75, 0x10,        //     Report Size (16)
-0x95, 0x06,        //     Report Count (6)
-0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0xC0,              //   End Collection
-                         // Report 3: Keys  // find #define HIDMAXBUTTONS 32 in config_sample.h
+    0x05, 0x01,          // Usage Page (Generic Desktop)
+    0x09, 0x08,          // Usage (Multi-Axis)
+    0xA1, 0x01,          // Collection (Application)
+                         // Report 1: Translation
+    0xA1, 0x00,          //   Collection (Physical)
+    0x85, 0x01,          //     Report ID (1)
+    0x16, 0xA2, 0xFE,    //     Logical Minimum (-350)
+    0x26, 0x5E, 0x01,    //     Logical Maximum (350)
+    0x36, 0x88, 0xFA,    //     Physical Minimum (-1400)
+    0x46, 0x78, 0x05,    //     Physical Maximum (1400)
+    0x55, 0x0C,          //     Unit Exponent (-4)
+    0x65, 0x11,          //     Unit (System: SI Linear, Length: Centimeter)
+    0x09, 0x30,          //     Usage (X)
+    0x09, 0x31,          //     Usage (Y)
+    0x09, 0x32,          //     Usage (Z)
+    0x09, 0x33,          //     Usage (Rx)
+    0x09, 0x34,          //     Usage (Ry)
+    0x09, 0x35,          //     Usage (Rz)
+    0x75, 0x10,          //     Report Size (16)
+    0x95, 0x06,          //     Report Count (6)
+#ifdef ADV_HID_REL    // see Advanced HID settings in config_sample.h
+    0x81, 0x06,       //     Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+#else
+    0x81, 0x02, // Input (variable,absolute)
+#endif
+    0xC0,                //   End Collection
+                         // Report 3: Keys  
     0xa1, 0x00,          // Collection (Physical)
     0x85, 0x03,          //  Report ID (3)
     0x15, 0x00,          //   Logical Minimum (0)
     0x25, 0x01,          //    Logical Maximum (1)
     0x75, 0x01,          //    Report Size (1)
-    0x95, HIDMAXBUTTONS, //    Report Count (32)
+    0x95, 0x20,          //    Report Count (32)
     0x05, 0x09,          //    Usage Page (Button)
-    0x19, 1,             //    Usage Minimum (Button #1)
-    0x29, HIDMAXBUTTONS, //    Usage Maximum (Button #24)
+    0x19, 0x01,          //    Usage Minimum (Button #1)
+    0x29, 0x20,          //    Usage Maximum (Button #24)
     0x81, 0x02,          //    Input (variable,absolute)
     0xC0,                // End Collection
                          // Report 4: LEDs
@@ -81,7 +84,7 @@ static const uint8_t SpaceMouseReportDescriptor[] PROGMEM = {
     0x75, 0x07,          //     Report Size (7)
     0x91, 0x03,          //     Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
     0xC0,                //   End Collection
-    0xc0              // END_COLLECTION
+    0xC0                 // END_COLLECTION
 };
 
 #define USBControllerInterface pluggedInterface
@@ -134,7 +137,7 @@ private:
 
 protected:
     uint8_t endpointTypes[2];
-    uint8_t protocol; 
+    uint8_t protocol;
     uint8_t idle;
 
     int getInterface(uint8_t *interfaceNumber);
